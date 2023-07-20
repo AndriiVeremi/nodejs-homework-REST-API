3d96eceda77ab474174936b37e2f21887ea9b1e7
Andrii Veremii <dashuk10@gmail.com>
2023-07-20 17:25:42 +0200

    HW 3

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,10 @@
 const HttpError = require("./HttpError");
+const ctrlWrapper = require("./ctrlWrapper")
+const handleMongooseError = require("./HandleMongooseError");
+
 
 module.exports = {
   HttpError,
+  ctrlWrapper,
+  handleMongooseError,
 };
