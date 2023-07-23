b0233f5234ba7b5f100af776d357093f2da254da
Andrii Veremii <dashuk10@gmail.com>
2023-07-23 20:27:16 +0200

    HW4

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+
+const { handleMongooseError } = require("../helpers");
+
+const emailRegexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+const subscripVersion = ["starter", "pro", "business"];
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    subscription: {
+      type: String,
+      enum: subscripVersion,
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const User = model("user", userSchema);
+
+module.exports = User;
+
+
+
+
