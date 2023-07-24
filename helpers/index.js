acb3f4189f19fcc3c2a8316df068999c99cb7966
Andrii Veremii <dashuk10@gmail.com>
2023-07-24 22:12:15 +0200

    HW 5

--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,12 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper")
 const handleMongooseError = require("./HandleMongooseError");
+const jimpAvatar = require("./jimpAvatar");
 
 
 module.exports = {
   HttpError,
   ctrlWrapper,
   handleMongooseError,
+  jimpAvatar,
 };
